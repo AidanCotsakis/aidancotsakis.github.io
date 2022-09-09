e007fb87cfb54d8788ed4e7e1f5690a8a82afddc
AidanCotsakis <47440253+AidanCotsakis@users.noreply.github.com>
2022-09-09 00:05:43 -0700

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,158 @@
+
+function bombbrawl(){
+	document.getElementById("bannerImg").src = "images/bombbrawlBanner.png";
+	document.getElementById("tabImg1").src = "images/bombbrawlScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/bombbrawlScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/aqYERoT8_1g?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Bomb Brawl";
+	document.getElementById("tabDescription").textContent = "A game where you fight another player in various bomb related gamemodes, including dropping bombs, throwing grenades, and avoiding falling bombs. Each with a single player and multiplayer option, locally on the same device.";
+	document.getElementById("tabWIL1").textContent = "This was my first game with a proper GUI in pygame. Every step of the way presented a challenge, including drawing sprites and shapes to the screen, animation, user input, movement, gravity, collisions, menus, and so much more.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2018/10/10";
+}
+
+function recoil(){
+	document.getElementById("bannerImg").src = "images/recoilBanner.png";
+	document.getElementById("tabImg1").src = "images/recoilScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/recoilScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/sXZ7cKKqryc?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Recoil";
+	document.getElementById("tabDescription").textContent = "A game where you must shoot incoming enemies, the catch is that the recoil of the bullets is your only form of movement.";
+	document.getElementById("tabWIL1").textContent = "As this was one of my first games, this was my first project that handles mouse input. I learned a lot more about momentum, rotating sprites, mouse input, collisions, and menus.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2018/10/29";
+}
+
+function takeover(){
+	document.getElementById("bannerImg").src = "images/takeoverBanner.png";
+	document.getElementById("tabImg1").src = "images/takeoverScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/takeoverScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/lCPdT8c89zM?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Takeover";
+	document.getElementById("tabDescription").textContent = "A board-like game where you fight against another player to take over the land with various types of weapons to prevent them from building.";
+	document.getElementById("tabWIL1").textContent = "Implementing multiple weapons required balancing to ensure all weapons are useful for their own reasons. This also taught me a lot about working with a grid as I had to handle building and destroying the territories on a grid.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2019/01/30";
+}
+
+function UTTT(){
+	document.getElementById("bannerImg").src = "images/UTTTBanner.png";
+	document.getElementById("tabImg1").src = "images/UTTTScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/UTTTScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/1SQ_y1YU3iA?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Ultimate Tic Tac Toe Minimax A.I.";
+	document.getElementById("tabDescription").textContent = "An AI that plays the much more strategic version of Tic Tac Toe, Ultimate Tic Tac Toe, I am yet to beat it. I also made a bot for a social media platform, Discord, that you can play against.";
+	document.getElementById("tabWIL1").textContent = "Implementing the minimax algorithm to search 7 moves in the future required a lot of optimization and challenges with this specific game. I also had to make the whole game first with a proper GUI and user input.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2020/03/15";
+}
+
+function morph(){
+	document.getElementById("bannerImg").src = "images/morphBanner.png";
+	document.getElementById("tabImg1").src = "images/morphScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/morphScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/zFeyc289Rmc?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Morph";
+	document.getElementById("tabDescription").textContent = "A game where you must navigate to the goal by sticking to walls and morphing the shape of your character to make your way around obstacles.";
+	document.getElementById("tabWIL1").textContent = "Making a dynamically shaped character was a new concept for me and forced me to rethink how I would implement something as basic as player movement. Drawing the screen from a simple tilesheet also made me rethink how I normally display the graphics.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2020/03/19";
+}
+
+function basketball(){
+	document.getElementById("bannerImg").src = "images/basketballBanner.png";
+	document.getElementById("tabImg1").src = "images/basketballScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/basketballScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/0Mvt9OSv8ps?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Basketball Physics Simulator";
+	document.getElementById("tabDescription").textContent = "A program that simulates the physics of a 2D basketball from scratch.";
+	document.getElementById("tabWIL1").textContent = "Collisions were the hardest to implement, and made me think about some fundamental concepts of physics in a new light. This was especially challenging as at the time this was written, I had not taken any courses on physics and had to figure everything out for myself.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2020/04/20";
+}
+
+function farming(){
+	document.getElementById("bannerImg").src = "images/farmingBanner.png";
+	document.getElementById("tabImg1").src = "images/farmingScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/farmingScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/y8WPLMUKazA?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Turn Based Farming Puzzle Game";
+	document.getElementById("tabDescription").textContent = "A puzzle game where you must optimize your moves to grow enough crops before the crow gets to them first.";
+	document.getElementById("tabWIL1").textContent = "I learned a lot about pathfinding as the crow tries to take the shortest path to its targeted crop. I also spent a lot of time polishing the games art, animations, gameplay, and general user experience.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2021/01/24";
+}
+
+function cahactus(){
+	document.getElementById("bannerImg").src = "images/cahactusBanner.png";
+	document.getElementById("tabImg1").src = "images/cahactusScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/cahactusScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/AUz-LQNWIHo?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Cahactus";
+	document.getElementById("tabDescription").textContent = "A LAN hosted multiplayer game where cacti fight each other.";
+	document.getElementById("tabWIL1").textContent = "This program taught me a lot about sockets, sending data from one computer to another, and generally a lot about servers and clients. I additionally had to optimize the information I sent to minimize lag.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2021/10/09";
+}
+
+function threeDrender(){
+	document.getElementById("bannerImg").src = "images/3DrenderBanner.png";
+	document.getElementById("tabImg1").src = "images/3DrenderScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/3DrenderScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/f7ZxreoWg5k?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "3D Renderer";
+	document.getElementById("tabDescription").textContent = "A program that can render a set of 3D coordinates on a 2D screen.";
+	document.getElementById("tabWIL1").textContent = "Implementing the math that converts 3D coordinates to 2D forced me to understand syntax I was not familiar with. I also had to learn new mathematical principles that my schooling had not covered at the time, such as matrices.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2022/02/06";
+}
+
+function breakless(){
+	document.getElementById("bannerImg").src = "images/breaklessBanner.png";
+	document.getElementById("tabImg1").src = "images/breaklessScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/breaklessScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/Z54kzAxAgeM?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Breakless";
+	document.getElementById("tabDescription").textContent = "A puzzle game where you must get to the end of each level, where you can only stop moving by hitting a wall.";
+	document.getElementById("tabWIL1").textContent = "Implementing a level builder that allows you to easily plan, build, and test levels required a lot of user interaction, teaching me a lot about ways to fluently switch between tools and modes. I also focused on polishing the graphics.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2022/06/26";
+}
+
+function passwordmanager(){
+	document.getElementById("bannerImg").src = "images/passwordmanagerBanner.png";
+	document.getElementById("tabImg1").src = "images/passwordmanagerScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/passwordmanagerScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/OU8SmCQL6L8?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Password Manager";
+	document.getElementById("tabDescription").textContent = "A password manager that encrypts and stores all your personal data locally, with many functions such as copy and paste and auto generating passwords!";
+	document.getElementById("tabWIL1").textContent = "I created and implemented a secure encryption algorithm from scratch, primarily using a binary xor operation as well as some other basic operations. I also made a polished GUI for it all with pygame.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2022/06/29";
+}
+
+function smallsnake(){
+	document.getElementById("bannerImg").src = "images/smallsnakeBanner.png";
+	document.getElementById("tabImg1").src = "images/smallsnakeScreenshot1.png";
+	document.getElementById("tabImg2").src = "images/smallsnakeScreenshot2.png";
+	document.getElementById("iframe").src = "https://www.youtube-nocookie.com/embed/Y_Etp18KYX0?rel=0&mute=1";
+	document.getElementById("tabTitle").textContent = "Snake in 9 Lines of Code";
+	document.getElementById("tabDescription").textContent = "A fully functional game of snake with movement, collisions, a distinct head, different colours, and more! All written in 9 lines of code.";
+	document.getElementById("tabWIL1").textContent = "As python heavily uses indentation in its syntax, I had to come up with many solutions to unique problems this challenge presented to minimize the number of lines used.";
+	document.getElementById("tabWIL2").textContent = "";
+	document.getElementById("tabWIL3").textContent = "";
+	document.getElementById("tabInfo").textContent = "Python - 2022/08/14";
+}
+
+smallsnake()
